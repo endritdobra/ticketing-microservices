e753c63880c1f6b9195b4e6d06c0337044353e41
endritdobra <endrit.dobra@gmail.com>
2025-11-19 14:09:09 +0100

    Add order-cancelled listener and enhance ticket update validation
    - Implement `OrderCancelledListener` to handle order cancellation events.
    - Update ticket model and routes to prevent updates to reserved tickets.
    - Improve tests to validate new restrictions and event publications.
    - Register new listener in application startup.

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import {app} from "../../app";
 import mongoose from "mongoose";
 import {natsWrapper} from "../../nats-wrapper";
+import {Ticket} from "../../models/tickets";
 
 it("returns a 404 if the provided id does not exist", async () => {
     const id = new mongoose.Types.ObjectId().toHexString();
@@ -69,4 +70,23 @@ it("publishes an event", async () => {
     }).expect(200);
 
     expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it("", async () => {
+    const cookie = global.signin();
+    const response = await request(app).post(`/api/tickets`).set("Cookie", cookie).send({
+        title: "asdfg",
+        price: 20
+    });
+
+    const ticket = await Ticket.findById(response.body.id);
+    ticket!.set({orderId: new mongoose.Types.ObjectId().toHexString()});
+    await ticket!.save();
+
+    await request(app).put(`/api/tickets/${response.body.id}`).set("Cookie", cookie).send({
+        title: "New title",
+        price: 100
+    }).expect(400);
+
+
 });
