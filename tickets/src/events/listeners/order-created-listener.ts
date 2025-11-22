f6df906b63c4a229b6c6d514675d1bcd74865bd6
endritdobra <endrit.dobra@gmail.com>
2025-11-22 07:49:24 +0100

    Refactor `order-created-listener` for improved readability and consistent formatting
    - Adjusted import statements and formatting for coherence.
    - Fixed incorrect `throw` error message syntax.
    - Added proper typing for `subject` property.

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,32 +1,30 @@
-import {Listener, OrderCreatedEvent, Subjects} from "@rallycoding/common";
-import {queueGroupName} from "./queue-group-name";
-import {Message} from "node-nats-streaming";
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@rallycoding/common';
+import { queueGroupName } from './queue-group-name';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 import {Ticket} from "../../models/tickets";
-import {TicketUpdatedPublisher} from "../publishers/ticket-updated-publisher";
-import {natsWrapper} from "../../nats-wrapper";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
-    readonly subject = Subjects.OrderCreated;
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
-    async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
         const ticket = await Ticket.findById(data.ticket.id);
 
         if (!ticket) {
-            throw new Error("Ticket not found.");
+            throw new Error('Ticket not found');
         }
 
-        ticket.set({orderId: data.id});
+        ticket.set({ orderId: data.id });
 
         await ticket.save();
-
         await new TicketUpdatedPublisher(this.client).publish({
             id: ticket.id,
             price: ticket.price,
             title: ticket.title,
             userId: ticket.userId,
             orderId: ticket.orderId,
-            version: ticket.version
+            version: ticket.version,
         });
 
         msg.ack();
